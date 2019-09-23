e044a9ba6b9bba8baea03a6a1beb9a55fb07276c
Tetiana <tatka777t@gmail.com>
2019-09-23 20:28:36 +0100

    2 home work changes

--- HW-2/src/task-4.js
+++ HW-2/src/task-4.js
@@ -3,7 +3,7 @@ const formatString = function(string) {
     return string;
   }
   string.length > 40;
-  return `${string}...`;
+  return string.substring(0, 40) + `...`;
 };
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
