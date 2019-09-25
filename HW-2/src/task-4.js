3d052a233ca32184c05be9fd68dcc5d5ba936e71
Tetiana <tatka777t@gmail.com>
2019-09-25 20:24:22 +0100

    last commit

--- HW-2/src/task-4.js
+++ HW-2/src/task-4.js
@@ -1,9 +1,9 @@
 const formatString = function(string) {
   if (string.length <= 40) {
     return string;
+  } else {
+    return string.substring(0, 40) + `...`;
   }
-  string.length > 40;
-  return string.substring(0, 40) + `...`;
 };
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
