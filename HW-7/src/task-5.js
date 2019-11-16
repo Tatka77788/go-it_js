1134055ee9c38751de092ef42650c6a970f96784
Tetiana <tatka777t@gmail.com>
2019-11-16 10:05:32 +0200

    HM 7 changings

--- HW-7/src/task-5.js
+++ HW-7/src/task-5.js
@@ -2,11 +2,9 @@ const refs = {
   input: document.querySelector('input[type="text"]'),
   name: document.getElementById('name-output'),
 };
-refs.input.addEventListener('input', handleInput);
-
 function handleInput(e) {
   e.preventDefault();
-  const { target } = e;
-  console.log(target.value);
-  refs.name.textContent = target.value;
+  refs.name.textContent = e.target.value;
+  if (refs.input.value.length == 0) refs.name.textContent = 'незнакомец';
 }
+refs.input.addEventListener('input', handleInput);
